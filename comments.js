0e1b7b02db150cf595a4b1a5b99a405ca0b67a59
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-05-19 18:15:26 +0000

    created web servero

--- comments.js
+++ comments.js
@@ -1,12 +1,23 @@
-// create web server
-import express from 'express';
-
-const app = express();
-const port = 3000;
-
-// create comments array
-const comments = [
-  { username: 'Tammy', comment: 'lololol' },
-  { username: 'FishBoi', comment: 'I am a fish!' },
-];
+// Create web server
+// const http = require('http');
+// const fs = require('fs');
+// 
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         fs.readFile('index.html', 'utf8', (err, data) => {
+//             if (err) {
+//                 res.writeHead(404, { 'Content-Type': 'text/html' });
+//                 res.end('404 Not Found');
+//             } else {
+//                 res.writeHead(200, { 'Content-Type': 'text/html' });
+//                 res.end(data);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/html' });
+//         res.end('404 Not Found');
+//     }
+// });
+// 
+// server.listen(3000, '
 
