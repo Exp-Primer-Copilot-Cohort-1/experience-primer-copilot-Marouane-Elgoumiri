92a58fb94513dc6f1896d9da05c978007cc6a465
_MR1_ <96888594+Marouane-Elgoumiri@users.noreply.github.com>
2024-05-19 18:11:04 +0000

    created web server

---
+++ comments.js
@@ -0,0 +1,12 @@
+// create web server
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// create comments array
+const comments = [
+  { username: 'Tammy', comment: 'lololol' },
+  { username: 'FishBoi', comment: 'I am a fish!' },
+];
+
